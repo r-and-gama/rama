021caad7e2b21dfe49da41d8ef50d6ad067d8500
nicolas <nmarilleau@gmail.com>
2018-12-08 22:39:34 +0700

    download UI

--- R/zzz.R
+++ R/zzz.R
@@ -100,7 +100,8 @@ defpath <- function(path) {
   packageStartupMessage("Welcome to gamar v1.0!")
   packageStartupMessage("WARNING: GAMA 1.7.0 needs to be installed on your machine.")
   packageStartupMessage("Check www.gama-platform.org for installation instructions.")
-#  defpath("/Users/choisy/Applications/Gama")
+  download_gama_ui();
+  #  defpath("/Users/choisy/Applications/Gama")
 #  defpath("/Applications/Gama.app")
 
 
