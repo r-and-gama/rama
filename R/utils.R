2fe03533b8456a0bd2c2c7b3467176fd7aa9d20d
nicolas <nmarilleau@gmail.com>
2018-12-10 17:41:04 +0700

    rama setup

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,26 @@
 gama_repo <- function(repo = NULL) {
   if (! is.null(repo)) options(rama.repo = repo)
 }
+
+
+
+# download GAMA when necessary -------------------------------------------------
+#' @export
+setup_gama <- function() {
+  repeat {
+    print("Do you want to download the last version of Gama?")
+    answer <- toupper(readline(" [Y]es/[N]o/[A]lready done."))
+    if (answer[1] == "Y" | answer[1] == "N" | answer[1] == "A") break
+    else print("Sorry, I din't understand... try again")
+  }
+  if (answer[1] == "Y") {
+    gama_path <- download_gama()
+    defpath(gama_path)
+  }
+
+  if (answer[1] == "A") {
+    #   gama_path <- download_gama()
+    #    defpath(gama_path)
+  }
+
+}
