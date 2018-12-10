627c9af073437ecad922a4e4ce5860b28835f959
Nicolas Marilleau <nmarilleau@gmail.com>
2018-12-11 00:26:19 +0700

    go to setup

--- R/utils.R
+++ R/utils.R
@@ -36,17 +36,17 @@ download_gama <- function() {
   expDir  <- gama_local_distrib_path();
   path <- paste0(options("rama.temp_dir"), "/")
   distrib_file <- paste0(path, "downloaded_gama.zip")
-  export_dir <-  paste0(expDir, "/")
   if (! dir.exists(path)) dir.create(path, recursive = TRUE)
-  download.file(distrib, distrib_file, quiet = FALSE, mode = "wb", cacheOK = TRUE)
-  unzip(distrib_file, exdir = export_dir )
-  file.remove(distrib_file)
+
+ # download.file(distrib, distrib_file, quiet = FALSE, mode = "wb", cacheOK = TRUE)
+  unzip(distrib_file, exdir = expDir )
+  # file.remove(distrib_file)
   gama_app <- switch(get_os(),
                      "Darwin" = options("rama.default.gama.osx.appdir"),
                      "Window" = options("rama.default.gama.win.appdir"),
                      "linux" = options("rama.default.gama.linux.appdir")
   );
-  paste0(export_dir,gama_app);
+  paste0(expDir,gama_app);
 }
 
 # ------------------------------------------------------------------------------
