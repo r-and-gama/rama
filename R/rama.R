9e9767442356123c75c345f54e457bd9e7518d29
Marc Choisy <marc.choisy@ird.fr>
2018-12-11 12:20:29 +0700

    Deleting the rama.R file

--- R/rama.R
+++
@@ -1,8 +0,0 @@
-#' gamar: An interface to the GAMA simulation platform.
-#'
-#' The gamar package provides the function 'run' that can run a GAMA experiment
-#' as described in an XML file.
-#'
-#' @docType package
-#' @name rama-package
-NULL
