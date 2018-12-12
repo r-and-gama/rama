ae128e214deb90a2bc761e268a303008edbdf453
Lucie Contamin <contamin.lucie@gmail.com>
2018-12-12 22:23:41 +0700

    add test on NA value

--- R/new_functions.R
+++ R/new_functions.R
@@ -750,6 +750,7 @@ show_experiment <- function(file){
 is.experiment <- function(x) {
   attr <- setdiff(c("names", "row.names", "class", "model", "experiment",
                            "wkdir", "dic", "dic_rev"), names(attributes(x)))
-  class <- setdiff(class(exp1), c("data.frame", "experiment"))
+  class <- setdiff(class(x), c("data.frame", "experiment"))
+  if(any(is.na(x))) stop("An object `experiment` can not contain NA value.")
   length(c(attr, class)) == 0
 }
