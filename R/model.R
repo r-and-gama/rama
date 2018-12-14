44f527a360de880c77a274a0b889e0fa43930056
Lucie Contamin <contamin.lucie@gmail.com>
2018-12-14 10:50:26 +0700

    Update arguments names to exp

--- R/model.R
+++ R/model.R
@@ -4,23 +4,23 @@
 #' These functions allow to get and set the path to the \code{.gaml} file that
 #' contains the model of an \code{experiment} object.
 #'
-#' @param x An object of class \code{experiment}.
+#' @param exp An object of class \code{experiment}.
 #'
 #' @return The path to the \code{.gaml} file that contains the model definition.
 #'
 #' @examples
 #' exp1 <- load_experiment("sir", system.file("examples", "sir.gaml",
-#'                         package = "rama"))
+#'                        package = "rama"))
 #' model(exp1)
 #'
 #' @export
 #'
-model <- function(x) UseMethod("model")
+model <- function(exp) UseMethod("model")
 
 #' @rdname model
 #' @export
-model.default <- function(x) "Unknown class"
+model.default <- function(exp) "Unknown class"
 
 #' @rdname model
 #' @export
-model.experiment <- function(x) attributes(x)$model
+model.experiment <- function(exp) attributes(exp)$model
