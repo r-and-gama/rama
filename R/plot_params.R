22c0ecf21a8e256b1a85c7911c7f4dcd3d774753
Jean-daniel Zucker <jdzucker@gmail.com>
2018-12-15 19:37:54 +0700

    The bug for one experiment and one single replicated experiment is corrected.

--- R/plot_params.R
+++ R/plot_params.R
@@ -18,7 +18,7 @@
 #'df <-   data.frame(S0 = c(900, 800, 500), # this is a data frame of 3 lines
 #'                   I0 = c(100, 200, 500),
 #'                   R0 = 0,
-#'                   beta = 1.5,
+#'                   beta = c(1.4,1.5,1.6),
 #'                   gamma = .15,
 #'                   S = c(1,2,3),
 #'                   I = c(2,4,6),
@@ -44,31 +44,34 @@
 #' @export
 #'
 plot_params <- function(exp) {
-  if (nrow(exp) == 0) {
-    stop(
-      "There is no set of parameters for this experiment in this experiment.")
-  }
+  if (nrow(exp) == 0) return("There is no simulation in this experiment")
+  if (nrow(exp) == 1) return("There is only one simulation in this experiment so no ")
 
   allvar <- sapply(parameters(exp), var)
   allvar <- sort(allvar[ allvar != 0], decreasing = TRUE)
+  if (length(allvar) == 0) return(paste("There is only one set of parameters for these",nrow(exp),"experiments"))
+
   worthidx <- sapply(X = names(allvar), function(x) which(colnames(exp) == x))
   topidx <- if (length(worthidx) != 0) {
     worthidx[1:min(3, length(worthidx))]
     } else {
     0}
   n <- length(topidx)
-  if (n == 3) scatter3D(exp[, topidx[1]], exp[, topidx[2]], exp[, topidx[3]],
-                      xlab = colnames(exp)[topidx[1]],
-                      ylab = colnames(exp)[topidx[2]],
-                      zlab = colnames(exp)[topidx[3]],
-                      pch = 18, cex = 2,
-                      theta = 20, phi = 20
-                      )
 
+  # check n the number of parameters to be plotted
+  # if n is equal to 0
+  if (n == 0) stop(paste0("There is no parameters to plot in this experiment"))
+  if (n == 1) stripchart(exp[, topidx[1]], xlab = colnames(exp)[topidx[1]])
   if (n == 2) scatter2D(exp[, topidx[1]], exp[, topidx[2]],
                       xlab = colnames(exp)[topidx[1]],
                       ylab = colnames(exp)[topidx[2]])
-
-  if (n == 1) stripchart(exp[, topidx[1]], xlab = colnames(exp)[topidx[1]])
+  if (n == 3) scatter3D(exp[, topidx[1]], exp[, topidx[2]], exp[, topidx[3]],
+                        xlab = colnames(exp)[topidx[1]],
+                        ylab = colnames(exp)[topidx[2]],
+                        zlab = colnames(exp)[topidx[3]],
+                        pch = 18, cex = 2,
+                        theta = 20, phi = 20
+  )
+  # returns the parameters of the ploted parameter(s)
   return(topidx)
 }
