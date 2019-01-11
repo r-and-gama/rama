d605224bd6f3818cf4e9bfb4708b1d5a6a19e80b
meta00 <hobichhai@gmail.com>
2019-01-12 00:38:33 +0700

    improve save_to_gama

--- R/save_to_gama.R
+++ R/save_to_gama.R
@@ -4,9 +4,9 @@
 #' Save an object of class \code{experiment} to an XML file GAMA-compliant.
 #'
 #' @param exp An object of class \code{experiment}.
-#' @param parameter_xml_file name of xml parameter file. This file is created
-#'                           in the working dirctory of 'exp'. If not specified,
-#'                           name of 'exp' is used.
+#' @param parameter_xml_file name of XML parameter file. This file is created
+#'                           in the working dirctory of `exp`. If not specified,
+#'                           name of `exp`` is used.
 #'
 #' @importFrom XML xmlToList xmlParse xmlOutputDOM saveXML
 #' @importFrom purrr map2 pmap
@@ -70,52 +70,10 @@ save_to_gama.experiment <- function(exp, parameter_xml_file = "") {
     xmlFile$closeTag()
   })
 
-  # id_simulation <- 0
-  # for (row_id in 1:nrow(exp)) {
-  #   attrib <- c(id         = row_id,
-  #               seed       = exp[row_id, ]$seed,
-  #               finalStep  = exp[row_id, ]$tmax,
-  #               sourcePath = model(exp),
-  #               experiment = name(exp))
-  #   xmlFile$addTag("Simulation", attrs = attrib, close = FALSE)
-  #   xmlFile$addTag("Parameters", close = FALSE)
-  #   y <- parameters(exp[row_id, ])
-  #   for (col_id in 1:ncol(y)) {
-  #     param <- y[, col_id, drop = FALSE]
-  #     title <- substr(names(param), 3, nchar(names(param)))
-  #     title <- as.vector(attr(exp, "dic_rev")[title])
-  #     val <- param[1, 1]
-  #     m_type <- "STRING"
-  #     if (is.numeric(val)) {
-  #       if (is.integer(val)) {
-  #         m_type <- "INT"
-  #       } else m_type <- "FLOAT"
-  #     }
-  #     attribut <- c(name = title,
-  #                   type = m_type,
-  #                   value = val)
-  #     xmlFile$addTag("Parameter", attrs = attribut)
-  #   }
-  #   xmlFile$closeTag()
-  #   xmlFile$addTag("Outputs", close = FALSE)
-  #   y <- obs_rates(exp[row_id, ])
-  #   id_out <- 0
-  #   for (col_id in 1:ncol(y)) {
-  #     param <- y[, col_id, drop = FALSE]
-  #     title <- substr(names(param), 3, nchar(names(param)))
-  #     title <- as.vector(attr(exp, "dic_rev")[title])
-  #     val <- param[1, 1]
-  #     attribut <- c(id        = id_out,
-  #                   name = title,
-  #                   framerate = val)
-  #     id_out <- id_out + 1
-  #     xmlFile$addTag("Output", attrs = attribut)
-  #   }
-  #   xmlFile$closeTag()
-  #   xmlFile$closeTag()
-  # }
-  # xmlFile$closeTag()
-  saveXML(xmlFile$value(), paste0(output_dir(exp), "/", parameter_xml_file))
+  if (parameter_xml_file == "")
+    parameter_xml_file <-  paste0(name(exp), ".xml")
+  parameter_xml_file <- paste0(output_dir(exp), "/", parameter_xml_file)
+  saveXML(xmlFile$value(), file = parameter_xml_file)
   normalizePath(parameter_xml_file)
 }
 
