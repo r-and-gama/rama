129491fea3f057597c4f7fa65206a20f8d43dea5
Lucie Contamin <contamin.lucie@gmail.com>
2019-01-16 10:20:35 +0700

    Update NicolasPred example script

--- inst/examples/NicolasPred.R
+++ inst/examples/NicolasPred.R
@@ -1,50 +1,34 @@
-#install.packages("devtools")
-#devtools::install_github("choisy/gamar")
-library(gamar)
-defpath("/Applications/Gama.app")
-myModelPath <- "/Users/jdz/git/gamar/inst/examples"
-#(myModelFile <- paste0(system.file("examples",package="gamar"),"/cluster_ind.gaml"))
-(myModelFile <- paste0(myModelPath,"/NicolasPred.gaml"))
+# install and load the package
+devtools::install_github("r-and-gama/rama")
+library(rama)
 
-(experimentPREY <- getmodelparameter(modelfile = myModelFile,experimentname = "prey_predator"))
-(getparameternames(experimentPREY))
+# if necessary to configure GAMA path
+# defpath("/Applications/Gama.app")
 
-action_distance <- data.frame("action_distance"=c(100,200,300,400,500,600,700,800,900,1000,1500,2000,2500,3000,3500,4000,5500,6000,7000,8000,9000,10000,12000,14000,16000,20000,25000,30000,40000,50000))
-environment_size <-  data.frame("environment_size"=c(500,10000,15000,20000,30000,40000,50000))
-all_seed <-  data.frame("mseed"=seq(1,30,1))
+# to load the experiment
+exp <- load_experiment("prey_predator",
+                       system.file("examples", "NicolasPred.gaml",
+                                   package = "rama"))
 
 
-simulation_duration <- 5000;
+
+
+# to set the experiment
+exp$p_danger_distance <-  c(100, 200, 300, 400, 500, 600, 700, 800, 900,
+                            1000, 1500, 2000, 2500, 3000, 3500, 4000, 5500,
+                            6000, 7000, 8000, 9000, 10000, 12000, 14000, 16000,
+                            20000, 25000, 30000, 40000, 50000)
+exp$p_environment_size <- c(500, 10000, 15000, 20000, 30000, 40000, 50000)
+
+exp$seed<- seq(1, 30, 1)
+exp$tmax <- 5000
 # To test we set a shorter duration...
-simulation_duration <- 3;
+exp$tmax <- 3
 
-simulations <- merge(action_distance,environment_size,by=NULL)
-simulations <- merge(simulations,all_seed,by=NULL )
-sim2 <- simulations
+exp$r_mean_pop <- 1
+exp$r_micro_b <- 1
+exp$r_nb_predators <- 1
+exp$r_nb_preys <- 1
 
-#sim2 <- simulations[!simulations$min_distance_crasy>simulations$max_distance_crasy,]
-experiment1 <- experimentPREY
-experimentplan <- NULL
-
-for (i in 1:nrow(sim2))
-{
-  local_experiment <-  setparametervalue(experiment1,"danger_distance",sim2[i,"action_distance"])
-  local_experiment <-  setparametervalue(local_experiment,"environment_size",sim2[i,"environment_size"])
-  local_experiment <-  setparametervalue(local_experiment,"currentSeed",sim2[i,"mseed"])
-  local_experiment <-  setparametervalue(local_experiment,"simulation_id",i)
-  local_experiment <- setfinalstep(local_experiment,simulation_duration)
-
-  local_experiment <- setoutputframerate(local_experiment,"nb_predators",1)
-  local_experiment <- setoutputframerate(local_experiment,"nb_preys",1)
-  local_experiment <- setoutputframerate(local_experiment,"micro_b",1)
-  local_experiment <- setoutputframerate(local_experiment,"mean_pop",1)
-  local_experiment <- setmodelpath(local_experiment,"../models/cluster_ind.gaml")
-  local_experiment <- setsimulationid(local_experiment,i)
-  local_experiment <- setseed(local_experiment,sim2[i,"mseed"])
-
-  experimentPlan  <- list()
-  experimentPlan <- addtoexperimentplan(simulation = local_experiment,experimentplan = experimentPlan)
-  experimentPlan$Simulation$.attrs['id'] <- i
-  outFile <- paste0("/tmp/experiment/input_",i,".xml")
-  writemodelparameterfile(experimentPlan,outFile)
-}
+# to run the experiment (do not run)
+#out <- run_experiment(exp)
