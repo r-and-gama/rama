3af1c46933705aba42cad0ecdf7ecd1ec7a52bfb
Marc Choisy <marc.choisy@ird.fr>
2019-01-21 15:16:40 +0700

    Minors edits on load_experiment()

--- R/load_experiment.R
+++ R/load_experiment.R
@@ -124,14 +124,14 @@ load_experiment <- function(exp, model, dir = "") {
   test_schar(names(dic))
 
   out_attr <- get_attributes(out)
-  output <- as.data.frame(c(out_par, out_var, out_attr))
+
+  output <- as.data.frame(c(out_par, out_var, out_attr), stringsAsFactors = FALSE)
   output$gaml <- NULL
   output$experiment <- NULL
-  class(output) <- c("experiment", class(output))
-  attr(output, "model") <- as.character(unname(out_attr$gaml))
-  attr(output, "experiment") <- as.character(unname(out_attr$experiment))
-  wk_dir <- make_wkdir(dir, model)
-  attr(output, "wkdir") <- wk_dir
+  class(output) <- c("experiment", "data.frame")
+  attr(output, "model") <- unname(out_attr$gaml)
+  attr(output, "experiment") <- unname(out_attr$experiment)
+  attr(output, "wkdir") <- make_wkdir(dir, model)
   attr(output, "dic") <- dic
   attr(output, "dic_rev") <- setNames(names(dic), dic)
 
