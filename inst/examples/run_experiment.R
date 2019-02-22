52b15b8437f728974dbfca31c3181f4b2a978a0c
Lucie Contamin <contamin.lucie@gmail.com>
2019-02-22 10:18:07 +0700

    Update examples for run_experiment

--- inst/examples/run_experiment.R
+++ inst/examples/run_experiment.R
@@ -3,3 +3,9 @@ gaml_file <- system.file("models", "sir.gaml", package = "rama")
 exp1 <- load_experiment("sir", gaml_file, "sir")
 # run experiment
 out <- run_experiment(exp1)
+
+# for a more complexe example
+exp1 <- repl(exp1, 2)
+exp1$r_R <- 2:3
+output <- run_experiment(exp1)
+str(output)
