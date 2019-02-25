529e80d837ddb4251b21b1b72ba2dcc3aee13a63
meta00 <hobichhai@gmail.com>
2019-02-25 20:23:00 +0700

    change interface

--- R/run_experiment.R
+++ R/run_experiment.R
@@ -73,12 +73,20 @@ retrieve_results <- function(outfile, exp) {
 #' (\code{\link[rama]{call_gama}}) and returns a list of data frame, one by
 #' simulation.
 #'
-#' @param exp an XML file containing the experiment
-#' @param hpc numeric
+#' @param exp an XML file containing the experiment.
+#' @param hpc number of threads used by GAMA to run the experiment.
+#' @param save save the outputs to disk or not, default = FALSE.
+#' @param path directory to save the outputs, default = NULL.
+#'             If `save = TRUE` and `path` is not
+#'             specified, current working directory is used to save the outputs.
+#' @param display output images are saved or not, default = FALSE.
+#' @param append append outputs to experiment, default = TRUE. It is not possible
+#'                to set both `add_exp` and `save` as `FALSE`.
 #'
 #' @example inst/examples/run_experiment.R
 #' @export
-run_experiment <- function(exp, hpc = 1) {
+run_experiment <- function(exp, hpc = 1, save = FALSE, path = NULL,
+                          display = FALSE, append = TRUE) {
 
   if (!is.experiment(exp)) {
     stop("The argument `exp` is not an `experiment` object.")
@@ -104,11 +112,47 @@ run_experiment <- function(exp, hpc = 1) {
   out <- lapply(outfiles, retrieve_results, exp)
 
   # Correct NA observations
-
   out <- realexp(out, exp)
 
-  # deleting the "workspace" folder:
-  unlink("workspace", T, T)
-  # return output:
-  out
+  # after-run operations
+  if(isFALSE(save) && isFALSE(append))
+    stop(paste0("Outputs needs to be saved either in disk or in experiment object."))
+
+  if(isTRUE(save)){
+    if(is.null(path)){
+      path <-  paste0(getwd(), "/", name(exp))
+      warning(paste0("Outputs are saved to '", path, "' by default."))
+    }
+    dir <- path
+    if(file.exists(path)) {
+      i <- 0
+      repeat {
+        i <- i + 1
+        dir <- paste0(path, "_", i)
+        if (!file.exists(path)) break
+      }
+      warning(paste0("'", path, "' already exists. Outputs are saved in '",
+                     dir, "'."))
+    }
+    input <- paste0(dir, "input")
+    dir.create(input)
+    output <- paste0(dir, "output")
+    dir.create(output)
+    file.copy(parameter_xml_file, input)
+    file.copy(model(exp)$path, input)
+    file.copy(outfiles, output)
+  }
+
+  if(isTRUE(display)){
+    images <- paste0(dir, "/images")
+    dir.create(images)
+    if(file.exists(paste0(output_dir, "/images")))
+      file.copy(paste0(output_dir, "/images"), images, recursive = TRUE)
+  }
+
+  if(isTRUE(append))
+   # To do
+
+ # return experiment:
+  exp
 }
