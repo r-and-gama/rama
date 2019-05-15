f6506357750737321c3363a281ed20562f01233f
Lucie Contamin <contamin.lucie@gmail.com>
2019-05-15 17:58:18 +0700

    Update test load_experiment

--- tests/testthat/test_load_experiment.R
+++ tests/testthat/test_load_experiment.R
@@ -3,7 +3,4 @@ testthat::test_that(
     sir1[, ] <- NULL
     testthat::expect_equal(rama:::get_parameters(sir1), NULL)
     testthat::expect_equal(rama:::get_variables(sir1), NULL)
-    test <- rama:::make_df_dic(NULL)
-    testthat::expect_equal(dim(test$out), c(0, 0))
-    testthat::expect_equal(test$dic, NULL)
 })
