ab2e48f0b215f094704a21f5432c1fdb8517a012
meta00 <hobichhai@gmail.com>
2019-05-21 20:46:05 +0700

    fixed bug

--- R/list_experiments.R
+++ R/list_experiments.R
@@ -14,16 +14,21 @@ list_experiments <- function(file){
   gaml <- paste(readLines(file, warn = FALSE), collapse = "\n")
   exp_info <- gregexpr("\\nexperiment (.*?)\\{", gaml)
   exp_info <- exp_info[[1]]
+  if(any(exp_info == -1)){
+    return(NULL)
+  }
   exps <- lapply(seq_along(exp_info), function(x) {
     exp <- substr(gaml, exp_info[x],
                   exp_info[x] + attr(exp_info, "match.length")[x])
-    exp <- trimws(gsub("\n|experiment|\\{|\\n+$", "", exp))
+    exp <- trimws(gsub("\\nexperiment|\\{|\\n+$", "", exp))
     if (grepl("type:", exp)) {
       experiment <- trimws(substr(exp, 1, regexpr("type:", exp) - 1))
       type <- trimws(substr(exp, regexpr("type:", exp) + 5, nchar(exp)))
+      if(grepl("\\s+", type))
+        type <- trimws(substr(type, 1, regexpr("\\s+", type)))
       cbind(experiment, type)
     } else {
-      cbind("experiment" = exp, "type" = "gui")
+      cbind("experiment" = exp, "type" = "gui_default")
     }
   })
   exp_info <- as.data.frame(do.call(rbind, exps), stringsAsFactors = FALSE)
